f03debcc09f45325db7a74f20f73adb797699b6f
shturnev <sht_job@ukr.net>
2016-10-06 19:49:37 +0300

    +

--- 01/index.php
+++ 01/index.php
@@ -1,4 +1,25 @@
 <?php
 
+
 ?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8"/>
+    <title></title>
+    <link rel="shortcut icon" href=""/>
+    <link rel="stylesheet" type="text/css" media="all" href=""/>
+
+    <script type="text/javascript" src=""></script>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+
+</head>
+
+<body>
+
+
+</body>
+</html>
 
