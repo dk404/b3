95ab8e056dc686b6da606664e8b42f5171022fbc
Gritsay Yaroslav <Gritsayyaroslaw@gmail.com>
2016-10-07 15:34:29 +0300

    +

---
+++ 01/index1.php
@@ -0,0 +1,35 @@
+<?php
+
+//
+
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8"/>
+    <title>Знакомство с GIT (Контроль версий)</title>
+    <link rel="shortcut icon" href=""/>
+    <link rel="stylesheet" type="text/css" media="all" href=""/>
+
+    <script type="text/javascript" src=""></script>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+
+</head>
+
+<body>
+
+<ul>
+    <li>
+        <a href="https://yadi.sk/d/dEH8meADwNg45">Скачать openServer</a></li>
+    <li>
+        <a href="https://github.com/shturnev/b3" target="_blank">наш репозиторий на github</a>
+    </li>
+    <li>
+        <a href="http://www.syntevo.com/smartgit/download" target="_blank">скачать smartgit</a>
+    </li>
+</ul>
+
+</body>
+</html>
