b48f24b0f89787612fa9d8336f9425c6bf869787
grt <Gritsayyaroslaw@gmail.com>
2016-10-18 20:18:10 +0300

    +Создание баз данных -insert -delete -select

--- 06/DB.php
+++ 06/DB.php
@@ -62,5 +62,87 @@ class DB
     }
 
 
+    public function update($table, $arr, $where, $close = false)
+    {
 
+
+        if (!$this->db_connect->ping()) {
+            $this->connect();
+        }
+
+            $newArr = [];
+            foreach ($arr as $key => $value) {
+                $newArr[] = $key . "='" . $value . "'";
+            }
+
+
+            $resUpdate = $this->db_connect->query("UPDATE " . $table . " SET " . implode(",", $newArr) . " WHERE " . $where);
+            if (!$resUpdate) {
+                $result["error"] = $this->db_connect->errno;
+                $result["error_text"] = $this->db_connect->error;
+            }
+            $result["result"] = $resUpdate;
+
+            //закрываем соединение с бд
+            if($close){
+                $this->db_connect->close();
+            }
+
+            return $result;
+
+
+
+    }
+
+
+    public function delete($table, $where, $close = false)
+    {
+
+        if(!$this->db_connect->ping()){
+            $this->connect();
+    }
+
+
+        $resdelete = $this->db_connect->query("DELETE FROM ".$table." WHERE ".$where);
+        if(!$resdelete){
+            $result["error"] = $this->db_connect->errno;
+            $result["error_text"] = $this->db_connect->error;
+        }
+        $result["result"] = $resdelete;
+
+        //закрываем соединение с бд
+        if($close){
+            $this->db_connect->close();
+        }
+
+        //response
+        return $result;
+    }
+
+
+    public function select($sql, $close = false){
+
+        if(!$this->db_connect->ping()) {
+            $this->connect();
+        }
+
+        $resselect = $this->db_connect->query($sql);
+        if(!$resselect){
+            $result["error"] = $this->db_connect->connect_errno;
+            $result["error_text"] = $this->db_connect->connect_error;
+        }
+        else
+        {
+            $result["result"] = $resselect->fetch_all(MYSQLI_ASSOC);
+        }
+
+
+        //закрываем соединение с бд
+        if($close){
+            $this->db_connect->close();
+        }
+
+        //response      
+        return $result;
+    }
 }
