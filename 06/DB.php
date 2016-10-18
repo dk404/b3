05b1a3d6e1fbd7ef8018404d3cf7181156747df7
Helena0610 <luckybigdog@mail.ru>
2016-10-18 20:07:06 +0300

    Создали библиотеку для работы с БД функции из 05 урока преобразовали в методы 06 урока

--- 06/DB.php
+++ 06/DB.php
@@ -61,6 +61,84 @@ class DB
         return $result;
     }
 
+    public function update($table, $arr, $where, $close = false){
+
+        if(!$this->db_connect->ping()){
+            $this->connect();
+        }
+
+        $newArr = [];
+        foreach($arr as $key => $value){
+            $newArr[] = $key."='".$value."'";
+        }
 
 
-}
+
+        $resdb = $this->db_connect->query("UPDATE ".$table." SET ".implode(",", $newArr). " WHERE ".$where);
+        if(!$resdb){
+            $result["error"] = $this->db_connect->connect_errno;
+            $result["error_text"] = $this->db_connect->connect_error;
+        }
+        $result["result"] = $resdb;
+
+        //закрываем соединение с бд
+         if($close){
+             $this->db_connect->close();
+            }
+        //response
+        return $result;
+    }
+
+    public function delete($table, $where, $close = false){
+
+        if(!$this->db_connect->ping()){
+            $this->connect();
+        }
+
+
+        $resdb = $this->db_connect->query("DELETE FROM ".$table." WHERE ".$where);
+        if(!$resdb){
+            $result["error"] = $this->db_connect->connect_errno;
+            $result["error_text"] = $this->db_connect->connect_error;
+        }
+        $result["result"] = $resdb;
+
+        //закрываем соединение с бд
+        if($close){
+            $this->db_connect->close();
+        }
+        //response
+        return $result;
+    }
+
+    public function select($sql, $close = false){
+
+        if(!$this->db_connect->ping()){
+            $this->connect();
+        }
+
+
+        $resdb = $this->db_connect->query($sql);
+        if(!$resdb){
+            $result["error"] = $this->db_connect->connect_errno;
+            $result["error_text"] = $this->db_connect->connect_error;
+        }
+        else
+        {
+            $result["result"] = $resdb->fetch_all(MYSQLI_ASSOC);
+        }
+
+
+        //закрываем соединение с бд
+        if($close){
+            $this->db_connect->close();
+        }
+
+        //response
+        return $result;
+    }
+
+    }
+
+
+
