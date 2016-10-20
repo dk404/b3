1a032b53f3d1db9c02c03c7b083e5d311dd154dd
grt <Gritsayyaroslaw@gmail.com>
2016-10-20 19:23:08 +0300

    ++

--- 07/users.php
+++ 07/users.php
@@ -2,3 +2,17 @@
 
 ?>
 
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8"/>
+    <title></title>
+    <link rel="shortcut icon" href=""/>
+    <!--    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">-->
+    <link rel="stylesheet" type="text/css" media="all" href="css/style.css"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+<main class="list">
+    ul>li*5
+</main>
