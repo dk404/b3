68f98d299ad944085a8adfe262ddba20644f5599
shturnev <sht_job@ukr.net>
2016-10-20 12:36:13 +0300

    +

--- 06/DB.php
+++ 06/DB.php
@@ -81,5 +81,72 @@ class DB
     }
 
 
+    public function update($table, $arr, $where, $close = false){
+
+        $this->connect();
+
+        $newArr = [];
+        foreach($arr as $key => $value){
+            $newArr[] = $key."='".$value."'";
+        }
+
+        $resdb = $this->db_connect->query("UPDATE ".$table." SET ".implode(",", $newArr). " WHERE ".$where);
+        if(!$resdb){
+            $result["error"] = $this->db_connect->connect_errno;
+            $result["error_text"] = $this->db_connect->connect_error;
+        }
+        $result["result"] = $resdb;
+
+        //закрываем соединение с бд
+        if($close){ $this->db_connect; }
+
+        //response
+        return $result;
+    }
+
+
+    public function delete($table, $where, $close = false){
+
+        $this->connect();
+
+
+        $resdb = $this->db_connect->query("DELETE FROM ".$table." WHERE ".$where);
+        if(!$resdb){
+            $result["error"] = $this->db_connect->connect_errno;
+            $result["error_text"] = $this->db_connect->connect_error;
+        }
+        $result["result"] = $resdb;
+
+        //закрываем соединение с бд
+        if($close){ $this->db_connect; }
+
+        //response
+        return $result;
+    }
+
+
+    public function select($sql, $close = false){
+
+        $this->connect();
+
+
+        $resdb = $this->db_connect->query($sql);
+        if(!$resdb){
+            $result["error"] = $this->db_connect->connect_errno;
+            $result["error_text"] = $this->db_connect->connect_error;
+        }
+        else
+        {
+            $result["result"] = $resdb->fetch_all(MYSQLI_ASSOC);
+        }
+
+        //закрываем соединение с бд
+        if($close){ $this->db_connect; }
+
+        //response
+        return $result;
+    }
+
+
 
 }
--- 06/test.php
+++ 06/test.php
@@ -7,5 +7,7 @@ $resDb1 = $DB->insert("users", ["user_name" => "uuuu1", "pass" => "1111"]);
 $resDb2 = $DB->insert("users", ["user_name" => "uuuu2", "pass" => "1111"], true);
 $resDb3 = $DB->insert("users", ["user_name" => "uuuu3", "pass" => "1111"], true);
 
+$resSelect = $DB->select("SELECT * FROM users");
+
 $ddd = 1;
 
