4a057b1bb427d1386d590daf489e71e748448836
shturnev <sht_job@ukr.net>
2016-10-25 14:22:19 +0300

    + crypt & sweetAlert

---
+++ testing/test.php
@@ -0,0 +1,10 @@
+<?php
+
+$pass = "Семен семёныч";
+
+$hesh = password_hash($pass, PASSWORD_DEFAULT);
+$res = password_verify($pass, '$2y$10$Cr5s8RscJT1kMvaUkz6O7.r4nISBu.BWT6pXmD3PNoAg3XKCi399.');
+$dfdf = 1;
+
+?>
+
