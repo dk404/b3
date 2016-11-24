9f595092e867967d901aa756a168aa1bdf2ead0d
Helena0610 <luckybigdog@mail.ru>
2016-11-24 20:09:21 +0200

    создаём новую регистрацию

--- maket1/DB.sql
+++ maket1/DB.sql
@@ -9,6 +9,20 @@
 /*!40101 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Dumping structure for таблиця maket1.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `ID` int(3) NOT NULL AUTO_INCREMENT,
+  `email` varchar(80) NOT NULL DEFAULT '0',
+  `pass` varchar(20) NOT NULL DEFAULT '0',
+  `token` varchar(20) NOT NULL DEFAULT '0',
+  `confirm_email` int(1) NOT NULL DEFAULT '0',
+  `status` int(1) NOT NULL DEFAULT '0',
+  `date` bigint(15) NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 -- Dumping data for table maket1.users: ~0 rows (приблизно)
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
--- maket1/register.php
+++ maket1/register.php
@@ -4,7 +4,9 @@
 -------------------------------*/
 require_once("autoload.php");
 
+$DB      = new DB();
 $T_check    = new TextSecurity();
+
 
 /*------------------------------
 если была передана форма
@@ -18,7 +20,40 @@ if($_POST["method_name"] == "register")
             exit("Неверный e-mail");
         }
 
-        
+        //сначала проверим есть ли уже такой пользователь, если нет, то запишем
+        $sql = "SELECT * FROM users WHERE email = '".$_POST["email"]."'";
+        $resSelect = $DB->select($sql);
+
+        if ($resSelect["result"] and $resSelect["result"][0]["confirm_email"] == 1)
+        {
+            exit("Такой пользователь уже существует");
+        }
+        elseif($resSelect["result"] and $resSelect["result"][0]["confirm_email"] == 0)
+        {
+            exit("Такой пользователь уже существует, но необходимо подтвердить email");
+        }
+
+
+        //ФОРМИРУЕМ ДАННЫЕ ДЛЯ ЗАПИСИ В БАЗУ
+        $arr = [
+            "email"          => $_POST["email"]
+            ,"pass"          => password_hash($_POST["pass"], PASSWORD_DEFAULT)
+            ,"token"         => md5(time().rand())
+//            ,"confirm_email" =>
+//            ,"status"        =>
+            ,"date"          => time()
+        ];
+
+
+
+
+
+        //записываем данные в таблицу
+        $resInsert = $DB->insert("users", $arr, $close=false);
+
+
+
+
 
 
     }
@@ -28,6 +63,7 @@ if($_POST["method_name"] == "register")
 }
 
 ?>
+
 
 <!DOCTYPE html>
 <html lang="ru">
