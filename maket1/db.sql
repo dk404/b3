9cddc51d15fa3ee29586ddb14416fa219422deeb
Gritsayyaroslav <gritsayyaroslaw@gmail.com>
2016-11-24 19:15:08 +0200

    +

--- maket1/db.sql
+++ maket1/db.sql
@@ -9,9 +9,21 @@
 /*!40101 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
--- Dumping data for table maket1.makert1: ~0 rows (приблизно)
-/*!40000 ALTER TABLE `makert1` DISABLE KEYS */;
-/*!40000 ALTER TABLE `makert1` ENABLE KEYS */;
+
+-- Dumping structure for таблиця maket1.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `ID` int(3) NOT NULL AUTO_INCREMENT,
+  `email` varchar(50) NOT NULL DEFAULT '0',
+  `pass` varchar(50) NOT NULL DEFAULT '0',
+  `token` varchar(50) NOT NULL DEFAULT '0',
+  `confirm_email` int(1) NOT NULL DEFAULT '0',
+  `status` int(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table maket1.users: ~0 rows (приблизно)
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
